6b1f4aaaa9838f158ed6bda6b32f3018de8465de
Daniel Ochuba <danielochuba78@gmail.com>
2023-02-01 21:26:31 +0100

    Fix linter Errors

--- index.js
+++ index.js
@@ -3,27 +3,21 @@ const body = document.querySelector('body');
 const extBtn = document.getElementById('exit-btn');
 const modal = document.querySelector('#modal');
 const modalLinks = document.querySelector('.modal-nav-link');
-const disableModal = document.getElementsByClassName('modal');
 
-enableModal.addEventListener('click',() => {
- 
-    modal.classList.remove('display-none');
-    body.classList.add('none-scroll');
-       
+enableModal.addEventListener('click', () => {
+  modal.classList.remove('display-none');
+  body.classList.add('none-scroll');
 });
 
-extBtn.addEventListener('click',() => {
+extBtn.addEventListener('click', () => {
   modal.classList.add('display-none');
   body.classList.remove('none-scroll');
-    
 });
 
-
-for(let i = 0; i < modalLinks.length ; i++){
-        modalLinks[i].addEventListener('click', () => {
-            modal.classList.add('display-none');
-            body.classList.remove('none-scroll');
-            body.classList.add('scroll');
-        });
+for (let i = 0; i < modalLinks.length; i += 1) {
+  modalLinks[i].addEventListener('click', () => {
+    modal.classList.add('display-none');
+    body.classList.remove('none-scroll');
+    body.classList.add('scroll');
+  });
 }
-

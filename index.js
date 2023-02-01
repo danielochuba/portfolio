056ad90d7af8522f0d10c7e2212683e45243f93d
getssh <gtamrat33@gmail.com>
2023-02-01 21:39:49 +0300

    Enable modal button

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+const enableModal = document.querySelector('#menu');
+const body = document.querySelector('body');
+const extBtn = document.getElementById('exit-btn');
+const modal = document.querySelector('#modal');
+const modalLinks = document.querySelectorAll('.modal-nav-link');
+const disableModal = document.getElementsByClassName('modal');
 
-
-
-
-
+enableModal.addEventListener('click',() => {
+ 
+    modal.classList.remove('display-none');
+    body.classList.add('none-scroll');
+       
+});
